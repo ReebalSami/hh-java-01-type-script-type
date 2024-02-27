9cee2741e50e6ab34bdcd6d1472e84ec7e9488eb
aaronbecher <Aaronbecher@gmx.de>
2024-02-27 11:11:21 +0100

    Changed studentArray

--- src/main.ts
+++ src/main.ts
@@ -11,43 +11,39 @@ const displayGrades = (grades: Grade[]): string => {
     return grades.map(grade => (grade !== undefined ? grade : '*')).join(', ');
 };
 
-const student1: Student = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 20,
-    grades: [2, 'A', 3, 'B', undefined]
-};
+const students: Student[] = [
+    {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 20,
+        grades: [2, 'A', 3, 'B', undefined]
+    },
+    {
+        firstName: 'Jane',
+        lastName: 'Smith',
+        age: 22,
+        grades: ['C', 'B', 1, 2]
+    },
+    {
+        firstName: 'Alex',
+        lastName: 'Johnson',
+        age: 21,
+        grades: ['A', 1, 'B', 'C']
+    },
+    {
+        firstName: 'Emily',
+        lastName: 'Davis',
+        age: 19,
+        grades: [2, 'D', 'C', 1]
+    },
+    {
+        firstName: 'Chris',
+        lastName: 'Williams',
+        age: 23,
+        grades: ['B', 1, 2, 'A']
+    }
+];
 
-const student2: Student = {
-    firstName: 'Jane',
-    lastName: 'Smith',
-    age: 22,
-    grades: ['C', 'B', 1, 2]
-};
-
-const student3: Student = {
-    firstName: 'Alex',
-    lastName: 'Johnson',
-    age: 21,
-    grades: ['A', 1, 'B', 'C']
-};
-
-const student4: Student = {
-    firstName: 'Emily',
-    lastName: 'Davis',
-    age: 19,
-    grades: [2, 'D', 'C', 1]
-};
-
-const student5: Student = {
-    firstName: 'Chris',
-    lastName: 'Williams',
-    age: 23,
-    grades: ['B', 1, 2, 'A']
-};
-
-let studentArray:Student[] = [student1, student2, student3, student4, student5];
-let studentArrayTry: Student[] = []
 function showStudentInfos(student: Student) {
     console.log(student.firstName + " " + student.lastName + "(" + student.age
     + ")" + "\n" + "=".repeat(30) + "\nGrades: " + displayGrades(student.grades))
@@ -57,7 +53,8 @@ function showAllStudents(students: Student[]) {
     students.forEach(showStudentInfos)
 }
 
-showAllStudents(studentArray)
+showStudentInfos(students[0])
+showAllStudents(students)
 
 
 /*
